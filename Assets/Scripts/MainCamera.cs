fdb320b89e78e727ea8e07fec31e2c3b6f7e5a78
DESKTOP-I3RJQLK\Aaron <aaronligthart@hotmail.com>
2021-03-26 11:09:16 +0100

    fixed trees in main, added portal

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using VRolijk.Portals;
+
+namespace VRolijk
+{
+    public class MainCamera : MonoBehaviour
+    {
+
+        [SerializeField] Portal[] portals;
+
+        void Awake()
+        {
+            portals = FindObjectsOfType<Portal>();
+            RenderPipelineManager.beginCameraRendering += PreCull;
+        }
+
+        void PreCull(ScriptableRenderContext context, Camera cam)
+        {
+            for (int i = 0; i < portals.Length; i++)
+            {
+                portals[i].Render(context, cam);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            RenderPipelineManager.beginCameraRendering -= PreCull;
+        }
+    }
+}
