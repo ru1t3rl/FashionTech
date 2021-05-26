a97f49651852714eb1267e5d0d781bd05eec20d3
DESKTOP-UCVCHLP\aaronVR <aaronligthart@hotmail.com>
2021-05-26 11:29:48 +0200

    added more visuals to the debug

--- Assets/debugUIVisuals.cs
+++ Assets/debugUIVisuals.cs
@@ -49,13 +49,15 @@ public class debugUIVisuals : MonoBehaviour
         leftLegMin.text = "min: " + (walkinplace.minLeftUp + walkinplace.baseLeftOrientation.x);
         rightLegMax.text = "max: " + (walkinplace.getRightLegUpTime());
         rightLegMin.text = "min: " + (walkinplace.minRightUp + walkinplace.baseRightOrientation.x);
-        rightControllerY.text = "Ry: " + walkinplace.rightController.transform.localPosition.y; ;
+        rightControllerY.text = "Ry: " + walkinplace.rightController.transform.localPosition.y; 
         leftControllerY.text = "Ly: " +walkinplace.leftController.transform.localPosition.y; ;
         headY.text = "height: " + walkinplace.headHeight;
         hipsY.text = "hips: " + walkinplace.getHipHeight();
+        rightControllerY.transform.localPosition = new Vector3(rightControllerY.transform.localPosition.x,walkinplace.rightController.transform.localPosition.y*100 - 100,0);
+        leftControllerY.transform.localPosition = new Vector3(leftControllerY.transform.localPosition.x, walkinplace.leftController.transform.localPosition.y * 100 - 100, 0);
 
 
-}
+    }
 
     // Update is called once per frame
     void Update()
