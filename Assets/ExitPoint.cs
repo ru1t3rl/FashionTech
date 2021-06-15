55be8cbe5a724591dc767bff30ddd989c2f286df
DESKTOP-UCVCHLP\aaronVR <aaronligthart@hotmail.com>
2021-06-15 16:37:42 +0200

    first version of new exit point

--- Assets/ExitPoint.cs
+++ Assets/ExitPoint.cs
@@ -7,6 +7,13 @@ public class ExitPoint : MonoBehaviour
     public Animator realWorldAnimator;
     private GameObject realWorld;
     private GameManager gameManager;
+    public Material exitMaterial;
+    public float maxDistance = 5f;
+    private GameObject player;
+    private float distance;
+    public float minHeight = 1f;
+    public float maxHeight = 20f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -14,12 +21,20 @@ public class ExitPoint : MonoBehaviour
         realWorld = GameObject.Find("Realworld menu");
         realWorldAnimator = realWorld.GetComponent<Animator>();
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        player = GameObject.Find("VRCamera");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector3.Distance(player.transform.position, this.transform.position);
+        if(distance < maxDistance)
+        {
+            float lerpedDistance = Mathf.Lerp(minHeight, maxHeight, distance / maxDistance);
+            var poolHeight = Mathf.Clamp(lerpedDistance, minHeight, maxHeight);
+            exitMaterial.SetFloat("height", poolHeight);
+        }
     }
 
     void OnTriggerEnter(Collider other)
